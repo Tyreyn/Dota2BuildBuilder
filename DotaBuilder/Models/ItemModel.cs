09f6420f0d65dfc10d03ac71a3595d208d255977
Tyreyn <athosikish33@gmail.com>
2023-11-30 18:00:18 +0100

    Add images, try making tooltip, change builder layout

--- DotaBuilder/Models/ItemModel.cs
+++ DotaBuilder/Models/ItemModel.cs
@@ -5,5 +5,7 @@
         public int item_id { get; set; }
         public string item_name { get; set; }
         public string item_description { get; set; }
+
+        public string item_image_path { get; set; }
     }
 }
