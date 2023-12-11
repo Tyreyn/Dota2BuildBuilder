ea32425185bf6063c4fc7e60dd6bda24d8984475
Tyreyn <athosikish33@gmail.com>
2023-12-11 14:07:52 +0100

    add drag and drop service

---
+++ DotaBuilder/Components/Data/Classes/ItemClass.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DotaBuilder.Components.Data.Classes
+{
+    public class ItemClass
+    {
+        public int ItemID { get; set; }
+
+        public string ItemName { get; set; }
+
+        public string ItemButtonClass { get; set; }
+
+        public string ItemBackgroundPath { get; set; }
+
+        public string ItemDescription { get; set; }
+
+        public string ItemCssClass { get; set; }
+
+        public EventCallback<EventCallbackItemArgs> OnClickEvent { get; set; }
+
+        public EventCallback<EventCallbackItemArgs> OnHoverEvent { get; set; }
+
+        public EventCallbackItemArgs eventCallbackItemsArgs = new EventCallbackItemArgs();
+
+        public string GetImage(string imagePath)
+        {
+            //Console.WriteLine("[Loading] Image path: ./{0}", imagePath);
+
+            if (File.Exists(Path.Combine(System.IO.Directory.GetCurrentDirectory(), "wwwroot", imagePath)))
+            {
+                return "./" + imagePath;
+            }
+            else
+            {
+                return "./images/Unknown_Item_icon.webp";
+            }
+        }
+
+        public string GetToolTip(string tooltipItemName)
+        {
+            return tooltipItemName;
+        }
+    }
+}
---
+++ DotaBuilder/Components/Data/DragEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace DotaBuilder.Components.Data
+{
+    public class DragEventArgs : MouseEventArgs
+    {
+        public DataTransfer DataTransfer { get; set; }
+    }
+}
